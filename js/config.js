af29d968d6799105affe0cf0d41e13cd1356b94d
jhelooooo <acrisolo.personiv@gmail.com>
2026-05-16 22:20:15 +0800

    Use correct raw sales table name

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://vcunasmorofoxaszygqg.supabase.co";
 const SUPABASE_KEY = "sb_publishable_h8L8cbobNM6oJeAEDOZDsQ_9JCdch2w";
-const SALES_TABLE = "sales_data";
+const SALES_TABLE = "raw_sales";
 const TARGETS_TABLE = "brand_targets";
