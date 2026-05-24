24b3375ee96ee6dc1af2eda39819116788d095b3
jhelooooo <acrisolo.personiv@gmail.com>
2026-05-24 15:27:21 +0800

    Update supabase.js

--- assets/js/core/supabase.js
+++ assets/js/core/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL='https://dyfjfqwzxeisagiuaisk.supabase.co';
-const SUPABASE_KEY='REPLACE_WITH_SUPABASE_PUBLISHABLE_KEY';
+const SUPABASE_KEY='sb_publishable_mDeOOSE6RJ2gpd6FX74t-g_tzZF0LTa';
 
 const supabaseClient=window.supabase.createClient(
   SUPABASE_URL,
