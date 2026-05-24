f6c045646212ee3e0bf9cfbdcb030cd9f6a8b373
jhelooooo <acrisolo.personiv@gmail.com>
2026-05-24 16:52:24 +0800

    Create filter.js

---
+++ assets/js/core/filter.js
@@ -0,0 +1,20 @@
+window.appState = {
+
+  user:null,
+  profile:null,
+
+  sales:[],
+  filteredSales:[],
+
+  filters:{
+    fiscal_year:'ALL',
+    fiscal_month:'ALL',
+    sales_agent:'ALL'
+  },
+
+  activePage:'dashboard'
+
+};
+
+
+applyFilters()
