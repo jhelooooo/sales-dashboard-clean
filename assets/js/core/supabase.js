fcb96ac02352cfeedf09ea1ff2683455580fb98e
jhelooooo <acrisolo.personiv@gmail.com>
2026-05-24 15:23:36 +0800

    Add Supabase configuration

---
+++ assets/js/core/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL='https://dyfjfqwzxeisagiuaisk.supabase.co';
+const SUPABASE_KEY='REPLACE_WITH_SUPABASE_PUBLISHABLE_KEY';
+
+const supabaseClient=window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
